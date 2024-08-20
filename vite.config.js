16b60a6595614e03b7c9f7019315fecc6180d1f7
saiteja ranga <saitejaranga1@gmail.com>
2024-08-20 07:18:19 +0000

    second commit

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // Allows external access
+    port: 5173,       // Ensure it matches the port you are using
+  },
+})
+
